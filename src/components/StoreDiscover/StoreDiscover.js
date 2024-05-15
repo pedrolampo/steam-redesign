9df5b5738b62d7ecedff53891523150902e1b904
Pedro Sanchez <pedrolampo50@gmail.com>
2024-05-14 21:30:40 -0300

    discover page dev

--- src/components/StoreDiscover/StoreDiscover.js
+++ src/components/StoreDiscover/StoreDiscover.js
@@ -3,13 +3,26 @@ import './StoreDiscover.css';
 
 import DiscoverCard from '../DiscoverCard/DiscoverCard';
 import { games } from '../../utils/gameList';
+import { useSearchParams } from 'react-router-dom';
 
 export default function StoreDiscover() {
+  const [params, setParams] = useSearchParams();
+  console.log(params.entries);
+
   return (
     <div className="store-discover">
       <div className="main-content">
         <div className="store-section">
-          <h3>Pedrolampo's Discovery Feed</h3>
+          <h3
+            onClick={() =>
+              setParams({
+                term: 'value',
+                name: 'jorge',
+              })
+            }
+          >
+            Pedrolampo's Discovery Feed
+          </h3>
           <p>
             Recommendations just for you, based on the games you or your friends
             want and play.
